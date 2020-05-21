7dfe531c651a85829e334a6e1e01b8cf53ff3972
Herman Ho <hermanho@users.noreply.github.com>
2020-05-21 14:37:34 +0800

    Migrate to typescript

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
+  plugins: ["@typescript-eslint", "prettier", "react"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint",
+    "prettier",
+  ],
+  settings: {
+    react: {
+      version: "latest",
+    },
+  },
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-this-alias": [
+      "error",
+      {
+        allowedNames: ["_self"], // Allow `const self = this`; `[]` by default
+      },
+    ],
+  },
+};
---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: false,
+  arrowParens: "always",
+  endOfLine: "auto",
+};
