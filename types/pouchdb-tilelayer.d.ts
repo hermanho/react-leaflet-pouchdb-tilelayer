468d61842b002aa6fca30c6d2b8c1f5385382db8
Herman Ho <hermanho@users.noreply.github.com>
2020-05-21 14:43:55 +0800

    Update build script

--- types/pouchdb-tilelayer.d.ts
+++ types/pouchdb-tilelayer.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="react" />
-import { GridLayerProps } from "react-leaflet";
+import { TileLayerProps } from "react-leaflet";
 export declare class PouchDBTileLayerOptions {
     useCache: boolean;
     saveToCache: boolean;
@@ -8,7 +8,7 @@ export declare class PouchDBTileLayerOptions {
     cacheMaxAge: number;
     cacheNextZoomLevel: boolean;
 }
-export interface PouchDBTileLayerProps extends PouchDBTileLayerOptions, GridLayerProps {
+export interface PouchDBTileLayerProps extends PouchDBTileLayerOptions, TileLayerProps {
 }
-declare const _default: import("react").ComponentType<Pick<PouchDBTileLayerProps, "children" | "useCache" | "saveToCache" | "useOnlyCache" | "cacheFormat" | "cacheMaxAge" | "cacheNextZoomLevel" | "attribution" | "pane" | "tileSize" | "opacity" | "updateWhenIdle" | "updateWhenZooming" | "updateInterval" | "zIndex" | "bounds" | "minZoom" | "maxZoom" | "noWrap" | "className" | "keepBuffer">>;
-export default _default;
+declare const ReactPouchDBTileLayer: import("react").ComponentType<Pick<PouchDBTileLayerProps, "children" | "url" | "useCache" | "saveToCache" | "useOnlyCache" | "cacheFormat" | "cacheMaxAge" | "cacheNextZoomLevel" | "attribution" | "pane" | "tileSize" | "opacity" | "updateWhenIdle" | "updateWhenZooming" | "updateInterval" | "zIndex" | "bounds" | "minZoom" | "maxZoom" | "noWrap" | "className" | "keepBuffer" | "onloading" | "onload" | "ontileloadstart" | "ontileload" | "ontileunload" | "ontileerror" | "id" | "accessToken" | "maxNativeZoom" | "minNativeZoom" | "subdomains" | "errorTileUrl" | "zoomOffset" | "tms" | "zoomReverse" | "detectRetina" | "crossOrigin">>;
+export default ReactPouchDBTileLayer;
