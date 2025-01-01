a4b7fc1433b7509a94c86017623cd949faaaf41c
Herman Ho <hermanho@users.noreply.github.com>
2025-01-01 01:36:54 +0000

    fix: upgrade eslint to v9

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import reactPlugin from 'eslint-plugin-react';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import reactHooks from 'eslint-plugin-react-hooks';
+
+export default [
+  pluginJs.configs.recommended,
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat['jsx-runtime'],
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    ignores: ['dist', '.eslintrc.cjs'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+
+      parser: tseslint.parser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'prettier/prettier': 'error',
+
+      'react-refresh/only-export-components': [
+        'warn',
+        {
+          allowConstantExport: true,
+        },
+      ],
+
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+  eslintPluginPrettierRecommended,
+];
